8479ea0dd6575793d878e370561e4b4a173d22a4
Ilovealtapow <Jfloyd421@gmail.com>
2021-11-04 15:34:45 -0600

    main

--- config.py
+++ config.py
@@ -5,6 +5,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     
+    FLASK_APP = os.getenv('FLASK_APP')
+    FLASK_ENV = os.getenv('FLASK_ENV')
     SECRET_KEY = 'You will never guess..'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEPLOY_DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
